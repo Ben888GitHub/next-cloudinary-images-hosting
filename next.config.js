89afde53b1eaa21f9b6b621f4e13889a3daf7920
Benedict <benedictryan80@gmail.com>
2022-11-09 22:06:32 +0700

    first commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+	reactStrictMode: true,
+	swcMinify: true,
+	images: {
+		domains: ['res.cloudinary.com']
+		// domains: ['i.ibb.co', 'flowbite.com']
+		// loader: 'cloudinary',
+		// path: 'https://res.cloudinary.com/'
+	}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
